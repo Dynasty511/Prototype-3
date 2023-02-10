21b562b0324bcc36e76fc431e075fe978ec7078d
Dynasty511 <jukang@s.sfusd.edu>
2023-02-10 15:43:56 -0800

    Added moving left to obstacles and background

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
     public float jumpForce = 10;
     public float gravityModifier;
     public bool isOnGround = true;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -25,9 +26,10 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    private void onCollisionEnter(Collision collision) 
+    private void OnCollisionEnter(Collision collision) 
     {
         isOnGround = true;
     }
+
     
 }
